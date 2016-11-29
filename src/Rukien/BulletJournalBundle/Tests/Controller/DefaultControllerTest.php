64042da17ce0f65d78d7316ad1b83bf7c6ce3bbb
Rukien <julien.jean@froupit.org>
2016-11-30 00:00:51 +0100

    fixed tests

--- src/Rukien/BulletJournalBundle/Tests/Controller/DefaultControllerTest.php
+++ src/Rukien/BulletJournalBundle/Tests/Controller/DefaultControllerTest.php
@@ -1,11 +1,16 @@
 <?php
-
 namespace Rukien\BulletJournalBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
+/**
+ * @class DefaultControllerTest
+ */
 class DefaultControllerTest extends WebTestCase
 {
+  /**
+   * Verifies the API is up and running
+   */
   public function testIndex()
   {
     $client = static::createClient();
@@ -13,11 +18,15 @@ class DefaultControllerTest extends WebTestCase
     $this->assertEquals(200, $client->getResponse()->getStatusCode());
   }
 
+  /**
+   * Verifies the number of loaded tasks is correct
+   */
   public function testTaskList()
   {
     $client = static::createClient();
     $crawler = $client->request('GET', '/tasks');
     $tasks = json_decode($client->getResponse()->getContent(), true);
-    $this->assertEquals(count($tasks), 4);
+    $this->assertEquals(count($tasks), 5);
   }
 }
+
